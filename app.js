f9efb879bb3beddb7c0662c3b564b4cc0ce4cfce
Your Name <you@example.com>
2021-08-26 11:26:10 +0530

    v1

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ let items = [];
 
 app.set('view engine' , 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get('/' , (req , res) => {
     
     var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+    console.log(time);
     
     var options = {
         weekday: "long",
@@ -22,16 +25,17 @@ app.get('/' , (req , res) => {
     var days = today.toLocaleDateString("en-US" , options);
     
 
-    res.render('index' , {cDay: days , newListItem: items});
+    res.render('index' , {cDay: days , newListItem: items , time : time});
 });
 
 app.post('/' , (req , res) => {
    
     var newListItem = req.body.task;
+    if(newListItem != ""){
     items.push(newListItem);
-    res.redirect('/')
+    res.redirect('/')}
 })
 
-app.listen(3000 , () => {
+app.listen(process.env.PORT || 3000 , () => {
     console.log("the server is running at port 3000");
 })
